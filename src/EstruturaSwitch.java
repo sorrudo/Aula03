ca4da5838d7b4ce8278d6b91102d058729eec863
Guilherme Radan <radanguilherme@hotmail.com>
2021-06-28 17:34:10 -0300

    implementando o operador Switch

--- src/EstruturaSwitch.java
+++ src/EstruturaSwitch.java
@@ -13,6 +13,17 @@ public class EstruturaSwitch {
         opcao = leitor.nextInt();
 
         //Aqui precisamos de uma estrutura para validar a opção inserida.
+        switch (opcao) {
+            case 1:
+                System.out.println("Os seus ifs são demais");
+                break;
+            case 2:
+                System.out.println("A sua densidade óssea é incrivel");
+                break;
+            case 3:
+                System.out.println("Você está indo muito bem");
+                break;
+        }
 
         leitor.close();
 
